c1f5e0e0705f40867c9a7d8451115e35755754c8
imora <imora@student.21-school.ru>
2022-01-07 15:19:32 +0300

    Derivatives and ReLU was add

--- base/NeuronFunctions.cpp
+++ base/NeuronFunctions.cpp
@@ -9,7 +9,33 @@ double	NeuronFunctions::getDelta(double expectation, double result)
 	return (result - expectation);
 }
 
+double	NeuronFunctions::getWeightDelta(double delta, double weight)
+{
+	return (delta * weight);
+}
+
+double	NeuronFunctions::getNewWeight(double weight_delta, double learn_rate)
+{
+	return (weight_delta * learn_rate);
+}
+
 double	NeuronFunctions::getSigma(double value)
 {
 	return (1 / (1 + exp(-value)));
+}
+
+double	NeuronFunctions::getDerivativeSigma(double value)
+{
+	double sigma = getSigma(value);
+	return (sigma * (1 - sigma));
+}
+
+double	NeuronFunctions::getReLU(double value)
+{
+	return (fmax(0, value));
+}
+
+double	NeuronFunctions::getDerivativeReLU(double value)
+{
+	return (1);
 }
--- base/NeuronFunctions.hpp
+++ base/NeuronFunctions.hpp
@@ -6,6 +6,11 @@
 
 class NeuronFunctions {
 public:
-	static double getDelta(double expectation, double result);
-	static double getSigma(double value);
+	static double	getDelta(double expectation, double result);
+	double			getWeightDelta(double delta, double weight);
+	double			getNewWeight(double weight_delta, double learn_rate);
+	static double	getSigma(double value);
+	static double	getDerivativeSigma(double value);
+	static double	getReLU(double value);
+	static double	getDerivativeReLU(double value);
 };
