2e30e63403a167f1f8460f3359b271565b5a5274
Ivan <imora@student.21-school.ru>
2022-01-12 18:54:32 +0300

    Header for library add

---
+++ 42neurons.hpp
@@ -0,0 +1,11 @@
+//
+// Created by Irena Mora on 1/12/22.
+//
+
+#pragma once
+
+#include "network/NeuronNetwork.hpp"
+#include "neurons/NeuronIn.hpp"
+#include "neurons/NeuronDeep.hpp"
+#include "neurons/NeuronOut.hpp"
+#include "exceptions/NeuronException.hpp"
