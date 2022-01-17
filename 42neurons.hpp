8dfc07a77ff4f3c5f4f22f1151c95792b487d7f8
Ivan <imora@student.21-school.ru>
2022-01-17 22:06:33 +0300

    Header with doc realized

--- 42neurons.hpp
+++ 42neurons.hpp
@@ -20,54 +20,46 @@ typedef std::set<NeuronSimple *> t_SetToNeurons;
 typedef std::set<NeuronConnection> t_SetNeuronConnections;
 typedef std::set<NeuronConnection *> t_SetNeuronToConnections;
 
-class NeuronFunctions
-{
-private:
-	static double	getSigmoid(double value);
-	static double	getDerivativeSigmoid(double value);
-	static double	getReLU(double value);
-	static double	getDerivativeReLU(double value);
-public:
-	static double	getRandomWeight();
-	static double	getCorrection(double value, FunctionType function_type);
-	static double	getDerivativeCorrection(double value, FunctionType function_type);
-};
-
+//////////////////////////////////////////
+/// \brief Class for the inner workings of the library. Implements connections between multiple neurons.
+//////////////////////////////////////////
 class NeuronConnection
 {
-private:
-	double			weight;
-	double			learning_rate;
-	FunctionType	function_type;
-	NeuronSimple	*from;
-	NeuronSimple	*to;
 public:
-	NeuronConnection(NeuronSimple &from, NeuronSimple &to, FunctionType function_type, double learning_rate, double weight);
-	double	getWeight() const;
-	void	setWeight(double weight);
-	double	getLearningRate() const;
-	void	setLearningRate(double learning_rate);
-	FunctionType	getFunctionType() const;
-	void	setFunctionType(FunctionType function_type);
-	NeuronSimple	&getNeuronFrom() const;
-	void	setNeuronFrom(NeuronSimple &from);
-	NeuronSimple	&getNeuronTo() const;
-	void	setNeuronTo(NeuronSimple &to);
-	static bool	isAlreadyConnected(t_SetNeuronConnections &connections, NeuronSimple &from, NeuronSimple &to);
+	//////////////////////////////////////////
+	/// \brief Operator overload comparison.
+	//////////////////////////////////////////
 	friend bool	operator < (const NeuronConnection &first, const NeuronConnection &second);
 };
 
+//////////////////////////////////////////
+/// \brief Class for handling exceptions on library errors.
+//////////////////////////////////////////
 class NeuronException
 {
 private:
 	long	code;
 	std::string	message;
 public:
+	//////////////////////////////////////////
+	/// \brief Constructor.
+	/// \param code Error code.
+	/// \param message Error message.
+	//////////////////////////////////////////
 	NeuronException(long code, std::string message);
+	//////////////////////////////////////////
+	/// \brief Returns an error code.
+	//////////////////////////////////////////
 	long	getCode();
+	//////////////////////////////////////////
+	/// \brief Returns an error message.
+	//////////////////////////////////////////
 	std::string	getMessage();
 };
 
+//////////////////////////////////////////
+/// \brief Base class for all types of neurons: input, deep, output.
+//////////////////////////////////////////
 class NeuronSimple
 {
 private:
@@ -82,39 +74,89 @@ private:
 	void	addConnection(NeuronConnection &connection);
 	void	removeConnection(NeuronConnection &connection);
 public:
+	//////////////////////////////////////////
+	/// \brief Class destructor.
+	//////////////////////////////////////////
 	virtual ~NeuronSimple() {};
+	//////////////////////////////////////////
+	/// \brief Class constructor.
+	/// \param status Specify the initial status (value) for the neuron for further calculations.
+	//////////////////////////////////////////
 	NeuronSimple(double status = 0);
+	//////////////////////////////////////////
+	/// \brief Get the status (value) of a neuron.
+	//////////////////////////////////////////
 	double	getStatus() const;
+	//////////////////////////////////////////
+	/// \brief Set the status (value) of a neuron.
+	/// \param status Sets the status (value) for the neuron. Status (value) is directly involved in further calculations.
+	//////////////////////////////////////////
 	void	setStatus(double status);
+	//////////////////////////////////////////
+	/// \brief Returns the number of connections present in the neural network.
+	//////////////////////////////////////////
 	size_t	getCountConnections();
 	friend class NeuronNetwork;
 };
 
+//////////////////////////////////////////
+/// \brief Incoming neuron. In neural networks, only these classes store all input data.
+//////////////////////////////////////////
 class NeuronIn : public NeuronSimple
 {
 public:
+	//////////////////////////////////////////
+	/// \brief Class constructor.
+	/// \param status The status (value) of a neuron stores the input data. For example, it can store the amount of red color from rgb (from 0 to 255), or store the price of a currency.
+	//////////////////////////////////////////
 	NeuronIn(double status = 0);
 };
 
+//////////////////////////////////////////
+/// \brief Deep neuron. There is no way to set the status (value). The neural network sets it when learning/calculating on its own.
+/// Just add objects of this class to the neural network, connect them with other neurons properly, and your task will be solved much more efficiently.   
+//////////////////////////////////////////
 class NeuronDeep : public NeuronSimple
 {
 private:
 	using NeuronSimple::setStatus;
 public:
+	//////////////////////////////////////////
+	/// \brief Class constructor
+	//////////////////////////////////////////
 	NeuronDeep();
 };
 
+//////////////////////////////////////////
+/// \brief Deep neuron.
+/// It stores the prediction of the neural network after the calculation and you put the desired result during training in it.
+//////////////////////////////////////////
 class NeuronOut : public NeuronSimple
 {
 private:
 	using NeuronSimple::setStatus;
 	double	expectedStatus;
 public:
+	//////////////////////////////////////////
+	/// \brief Class constructor.
+	/// \param expectedStatus Desired answer (for training) by default.
+	//////////////////////////////////////////
 	NeuronOut(double expectedStatus = 0.0);
+	//////////////////////////////////////////
+	/// \brief Set the expected status for this neuron (the neural network will strive for this status during training; this is the learning process).
+	/// \param expectedStatus Desired answer (for training) by default.
+	//////////////////////////////////////////
 	void	setExpectedStatus(double expectedStatus);
+	//////////////////////////////////////////
+	/// \brief Returns the desired status you set earlier.
+	//////////////////////////////////////////
 	double	getExpectedStatus() const;
 };
 
+//////////////////////////////////////////
+/// \brief Neural network. This is where you load the neurons, this is where you create all the connections.
+/// This class trains and calculates the whole system.
+//////////////////////////////////////////
 class NeuronNetwork
 {
 private:
@@ -130,11 +172,25 @@ private:
 	void disableAllNeurons();
 	void computeNeuron(NeuronSimple *next, double resume);
 	void learnNeuron(NeuronSimple &neuron, double sigma_weight);
-
 public:
+	//////////////////////////////////////////
+	/// \brief Class destructor.
+	//////////////////////////////////////////
 	~NeuronNetwork();
+	//////////////////////////////////////////
+	/// \brief Returns whether a neuron exists in this neural network.
+	/// \param neuron Neuron that is being tested for existence  
+	//////////////////////////////////////////
 	bool isExistNeuron(NeuronSimple &neuron);
+	//////////////////////////////////////////
+	/// \brief Add a neuron to this neural network.
+	/// \param neuron The neuron we are testing.  
+	//////////////////////////////////////////
 	void addNeuron(NeuronSimple &neuron);
+	//////////////////////////////////////////
+	/// \brief Add neurons to this neural network.
+	/// \param container_neurons Any container containing the neurons we want to add.  
+	//////////////////////////////////////////
 	template <typename T>
 	void addNeurons(T &container_neurons)
 	{
@@ -147,12 +203,43 @@ public:
 			begin++;
 		}
 	}
+	//////////////////////////////////////////
+	/// \brief Removes a neuron from this neural network.
+	/// \param neuron Neuron to be deleted.  
+	//////////////////////////////////////////
 	void removeNeuron(NeuronSimple &neuron);
+	//////////////////////////////////////////
+	/// \brief Creating a connection between two neurons with a random weight.
+	/// \param from Initial neuron.
+	/// \param to Terminal neuron.
+	/// \param function_type Activation function type.
+	/// \param learning_rate Neural network learning rate. Usually has small fractional values. 
+	//////////////////////////////////////////
 	void createConnection(NeuronSimple &from, NeuronSimple &to, FunctionType function_type, double learning_rate);
+	//////////////////////////////////////////
+	/// \brief Creating a connection between two neurons.
+	/// \param from Initial neuron.
+	/// \param to Terminal neuron.
+	/// \param function_type Activation function type.
+	/// \param learning_rate Neural network learning rate. Usually has small fractional values.
+	/// \param weight Initial connection weight between two neurons.
+	//////////////////////////////////////////
 	void createConnection(NeuronSimple &from, NeuronSimple &to, FunctionType function_type, double learning_rate, double weight);
+	//////////////////////////////////////////
+	/// \brief Returns the number of neurons that are part of this neural network.
+	//////////////////////////////////////////
 	size_t getCountNeurons();
+	//////////////////////////////////////////
+	/// \brief Returns the number of connections contained in this neural network.
+	//////////////////////////////////////////
 	size_t getCountConnections();
+	//////////////////////////////////////////
+	/// \brief Make a prediction.
+	//////////////////////////////////////////
 	void compute();
+	//////////////////////////////////////////
+	/// \brief Trains a neural network.
+	//////////////////////////////////////////
 	void learn();
 };
 
